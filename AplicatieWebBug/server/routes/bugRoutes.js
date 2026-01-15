db53f4b2d0313c349bc0d4c6a97a96eabc5446b3
Andrei-abc <andreifloroian1@gmail.com>
2026-01-15 23:58:11 +0200

    a

--- AplicatieWebBug/server/routes/bugRoutes.js
+++ AplicatieWebBug/server/routes/bugRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const bugController = require('../controllers/bugController');
 
 // Rute pentru operatii CRUD pe bug-uri
-router.get('/', bugController.getAllBugs);
-router.post('/', bugController.createBug);
+// Rutele mai specifice trebuie sa vina inaintea celor generice
 router.get('/project/:projectId', bugController.getBugsByProject);
-router.get('/:id', bugController.getBugById);
-router.put('/:id', bugController.updateBug);
 router.put('/:id/assign', bugController.assignBug);
 router.put('/:id/resolve', bugController.resolveBug);
+router.get('/', bugController.getAllBugs);
+router.post('/', bugController.createBug);
+router.get('/:id', bugController.getBugById);
+router.put('/:id', bugController.updateBug);
 router.delete('/:id', bugController.deleteBug);
 
 module.exports = router;
