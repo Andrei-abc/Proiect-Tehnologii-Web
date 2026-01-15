073ab2482eef7b07f96de6614a237245e3be996c
Andrei-abc <andreifloroian1@gmail.com>
2026-01-15 23:37:58 +0200

    a

--- AplicatieWebBug/server/server.js
+++ AplicatieWebBug/server/server.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-a
+
 // Log simplu pentru fiecare cerere primita
 app.use((req, res, next) => {
   console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
